f670d0b3da8bd14c151ddd4c32b956076c77dd39
Don Thomas <donthomas20399@gmail.com>
2025-03-17 01:07:41 +0530

    feat(user SignIn, user signUp):Created components for user signup and signIn

---
+++ src/app/components/user/user-login/user-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-user-login',
+  imports: [RouterModule],
+  templateUrl: './user-login.component.html',
+  styleUrl: './user-login.component.css'
+})
+export class UserLoginComponent {
+
+}
--- src/app/components/user/user-routing.module.ts
+++ src/app/components/user/user-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'userLogin',pathMatch:'full'},
+  {path:'userLogin',component:UserLoginComponent},
+  {path:'userDashboard',component:UserDashboardComponent},
+  {path:'home',title:'CRM | Home',loadChildren:() => import('../home/home.module').then(m =>m.HomeModule)},
+  {path:'signup',title:'CRM | Signup',component:SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
